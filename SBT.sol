018b366eecf3abc1e67afcbf38c6e581b48cdf99
Yiju Jung <79932335+20Yiju@users.noreply.github.com>
2023-03-05 21:24:29 +0900

    Add get TotalTokenIdFromOwner function

--- SBT.sol
+++ SBT.sol
@@ -1,14 +1,15 @@
-// Try using this as URI ipfs://bafkreic6ov4qo4ucd4g4uuyve4h72nc4y2lg7ugtq3n3vxnfp3lojvtmdu
+// ipfs://bafkreic6ov4qo4ucd4g4uuyve4h72nc4y2lg7ugtq3n3vxnfp3lojvtmdu
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
-
 import "@openzeppelin/contracts@4.7.0/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts@4.7.0/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts@4.7.0/token/ERC721/extensions/IERC721Metadata.sol";
+import "@openzeppelin/contracts@4.7.0/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts@4.7.0/access/Ownable.sol";
 import "@openzeppelin/contracts@4.7.0/utils/Counters.sol";
 
-contract Web3ClubTourToken is ERC721, ERC721URIStorage, Ownable {
+contract SBT is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
@@ -18,16 +19,13 @@ contract Web3ClubTourToken is ERC721, ERC721URIStorage, Ownable {
      // Mapping from token ID to locked status
     mapping(uint256 => bool) _locked;
 
+    // Mapping owner address to token count 
+    mapping(address => uint256) private _balances;
+
     /// @notice Emitted when the locking status is changed to locked.
     /// @dev If a token is minted and the status is locked, this event should be emitted.
     /// @param tokenId The identifier for a token.
     event Locked(uint256 tokenId);
-
-    /// @notice Emitted when the locking status is changed to unlocked.
-    /// @notice currently SBT Contract does not emit Unlocked event
-    /// @dev If a token is minted and the status is unlocked, this event should be emitted.
-    /// @param tokenId The identifier for a token.
-    event Unlocked(uint256 tokenId);
 
     /// @notice Returns the locking status of an Soulbound Token
     /// @dev SBTs assigned to zero address are considered invalid, and queries
@@ -87,4 +85,30 @@ contract Web3ClubTourToken is ERC721, ERC721URIStorage, Ownable {
     {
         return super.tokenURI(tokenId);
     }
+
+    function tokenOfOwnerByIndex(address owner, uint256 index) public view returns (uint256) {
+        return tokenOfOwnerByIndex(owner, index);
+    }
+
+    function balanceOf(address owner) public view virtual override returns (uint256) {
+        require(owner != address(0), "ERC721: address zero is not a valid owner");
+        return _balances[owner];
+    }
+
+    function getTotalTokenIdFromOwner(address owner) public view returns (uint256[] memory) {
+        uint256 totalToken = _tokenIdCounter.current();
+        uint256[] memory tokenIds = new uint256[](totalToken);
+        uint256 j = 0;
+
+        for (uint256 i = 0; i < totalToken; i++) {
+            if(owner == ownerOf(i)){
+                tokenIds[j] = i;
+                j++;
+            } 
+            
+        }
+
+        return tokenIds;
+    }
+
 }
