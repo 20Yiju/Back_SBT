057b1610ff8af292721016985422581cec9e1452
Yiju Jung <79932335+20Yiju@users.noreply.github.com>
2023-04-07 23:11:32 +0900

    automatically bring current contract address

--- safeMint.js
+++ safeMint.js
@@ -1,20 +1,17 @@
 const Web3 = require('web3');
-const contractAbi = require('./build/contracts/SBT.json').abi;
-const contractAddress = '0xA6306613570cAD714F83f54084280c6136681C10'; // Replace with your contract address
+const contract = require('truffle-contract');
+const sbtArtifact = require('./build/contracts/SBT.json');
 
 const web3 = new Web3('http://127.0.0.1:7545'); // Replace with your RPC endpoint
-const myContract = new web3.eth.Contract(contractAbi, contractAddress);
+const sbtContract = contract(sbtArtifact);
+sbtContract.setProvider(web3.currentProvider);
 
-// Get the default account from Ganache
-web3.eth.getAccounts((err, accounts) => {
-  if (err) throw err;
-  const defaultAccount = accounts[0];
+const defaultAccount = '0x21d2c960c3c66C1256b7097Cde68008E3db050dA';
 
+// Get the deployed contract instance
+sbtContract.deployed().then((instance) => {
   // Call the safeMint() method with the `from` option
-  myContract.methods.safeMint('0x21d2c960c3c66C1256b7097Cde68008E3db050dA', 'ipfs://bafkreic6ov4qo4ucd4g4uuyve4h72nc4y2lg7ugtq3n3vxnfp3lojvtmdu').send({
-    from: defaultAccount,
-    gas: 3000000 // Optionally specify the gas limit
-  })
+  instance.safeMint('0x21d2c960c3c66C1256b7097Cde68008E3db050dA', 'ipfs://bafkreic6ov4qo4ucd4g4uuyve4h72nc4y2lg7ugtq3n3vxnfp3lojvtmdu', { from: defaultAccount, gas: 3000000 })
   .then((receipt) => {
     console.log("Transaction receipt:", receipt);
   })
