9ba76fd7458c57294a75b87d944038583ca96d2a
Yiju Jung <79932335+20Yiju@users.noreply.github.com>
2023-05-05 01:16:54 +0900

    Update data_success.js

--- data_success.js
+++ data_success.js
@@ -3,8 +3,8 @@ import { Buffer } from 'buffer';
 const img = require("./Eat.png");
 
 export async function tokenOffering(courseName, section, type, tokenName, totalScore) {
-  const projectId = '2PKNbqRWAqBWGvWrflkFT9uZmG3';
-  const projectSecret = '1167a7d29f2e9a311a0b154f9cac6241';
+  const projectId = 'api key';
+  const projectSecret = 'api key secrete';
   const ipfs = create({
         host: 'ipfs.infura.io',
         port: 5001,
